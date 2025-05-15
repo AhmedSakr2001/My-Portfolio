f3e7b261a3a285123afc4c766e3d1917e905913b
AhmedSakr2001 <sakr8403@gmail.com>
2025-05-15 15:05:49 +0300

    My Portfolio

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Theme Toggle
+document.getElementById("theme-toggle").addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+    const icon = this.querySelector("i");
+    icon.classList.toggle("fa-sun");
+    icon.classList.toggle("fa-moon");
+});
+
+
+
+
